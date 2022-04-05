2b758323b4e18660a2960b97671207276fe9320a
Nick Shockey <shockeynj@gmail.com>
2022-04-05 13:09:18 -0400

    You can now view the source code from the app
    Also some prelim functions that don't work yet

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from logging import root
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from tkinter.messagebox import showerror
+from tkinter import messagebox
+import webbrowser
 
 from img_scale import rescale_image
 
@@ -37,10 +38,10 @@ class App(tk.Tk):
         self.lbl_files = ttk.Label(text="Open some pixel art to resize it...")
         self.lbl_files.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-        self.btn_open_file = ttk.Button(text="Open file", width=12)
+        self.btn_open_file = ttk.Button(text="Open file", width=12, command=lambda: openSprite())
         self.btn_open_file.place(relx=0.35, rely=0.6, anchor=CENTER)
 
-        self.btn_open_folder = ttk.Button(text="Open folder", width=12)
+        self.btn_open_folder = ttk.Button(text="Open folder", width=12, command=lambda: openSpriteFolder())
         self.btn_open_folder.place(relx=0.65, rely=0.6, anchor=CENTER)
 
         # Menu bar ######################
@@ -49,13 +50,13 @@ class App(tk.Tk):
 
         # Options - Settings, Open output folder, Exit
         options_m = Menu(menubar, tearoff=0, activeborderwidth=3)
-        options_m.add_command(label='Open output folder...', command=self.destroy)
+        options_m.add_command(label='Open output folder...')
 
         # Export submenu
         export_submenu = Menu(options_m, tearoff=0, activeborderwidth=3)
-        export_submenu.add_radiobutton(label='PNG', command=self.destroy)
-        export_submenu.add_radiobutton(label='Static GIF', command=self.destroy)
-        export_submenu.add_radiobutton(label='JPEG', command=self.destroy)
+        export_submenu.add_radiobutton(label='PNG')
+        export_submenu.add_radiobutton(label='Static GIF')
+        export_submenu.add_radiobutton(label='JPEG')
         options_m.add_cascade(label='Export as...', menu=export_submenu)
 
         options_m.add_separator()
@@ -63,18 +64,40 @@ class App(tk.Tk):
 
         # Theme
         # theme_m = Menu(options_m, tearoff=0, activeborderwidth=3)
-        # theme_m.add_radiobutton(label='Theme1', command=self.destroy)
-        # theme_m.add_radiobutton(label='Theme2', command=self.destroy)
+        # theme_m.add_radiobutton(label='Theme1')
+        # theme_m.add_radiobutton(label='Theme2')
 
         # Help - About, Source code
         help_m = Menu(menubar, tearoff=0, activeborderwidth=3)
-        help_m.add_command(label='About', command=self.destroy)
-        help_m.add_command(label='Source code', command=self.destroy)
+        help_m.add_command(label='About', command=lambda: openAbout())
+        help_m.add_command(label='Source code', command=lambda: viewSource())
 
         # Add to window
         menubar.add_cascade(label='Options', menu=options_m)
         # menubar.add_cascade(label='Theme', menu=theme_m)
-        menubar.add_cascade(label='Help', menu=help_m) 
+        menubar.add_cascade(label='Help', menu=help_m)
+
+        def openSprite(path=''):
+            if path != '':
+                pass
+            else:
+                invalidFileError()
+        
+        def openSpriteFolder(path=''):
+            if path != '':
+                pass
+            else:
+                invalidFileError()
+        
+        def openAbout():
+            pass
+        
+        def viewSource():
+            url = "https://github.com/njshockey/generic-pixel-art-scaler"
+            webbrowser.open_new_tab(url)
+        
+        def invalidFileError():
+            messagebox.showerror("Error", "Please select a valid file.")
 
 # Driver
 if __name__ == "__main__":
