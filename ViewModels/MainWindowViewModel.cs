63b14176c3703f5f48989b3fce7acf49a0ffdee2
Nick Shockey <shockeynj@gmail.com>
2022-07-08 12:43:07 -0400

    Added C# files

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericPixelArtScaler.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericPixelArtScaler.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
