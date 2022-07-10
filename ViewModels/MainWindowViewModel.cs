f8e7aceca63e6bbc20b1eb0360e57ac3c51c5c70
Nick Shockey <shockeynj@gmail.com>
2022-07-10 15:52:54 -0400

    Can now open the about window

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,38 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+
+using GenericPixelArtScaler.Views;
 
 namespace GenericPixelArtScaler.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ReactiveWindow<MainWindowViewModel>
     {
+        public Interaction<AboutWindow, MenuStrip?> OpenAbout { get; }
         public string Greeting => "Welcome to Avalonia!";
+
+        public MainWindowViewModel()
+        {
+            OpenAbout = new Interaction<AboutWindow, MenuStrip?>();
+
+        }
+
+        public void OpenAboutWindow()
+        {
+            var aboutWindow = new AboutWindow();
+            aboutWindow.Show();
+        }
+
+        public void ShowSource()
+        {
+            Console.WriteLine("Wow");
+        }
     }
 }
